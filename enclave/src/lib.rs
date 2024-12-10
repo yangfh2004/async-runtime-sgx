9aac60638b7c0d109c105761ffc140a94d46ff6e
Fanghao Yang <fyang@dexlabs.xyz>
2024-12-10 16:40:47 -0500

    impl

--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -25,16 +25,27 @@ extern crate sgx_types;
 
 #[cfg(target_vendor = "teaclave")]
 mod exception;
+use anyhow::{Context, Result};
+use bytes::Bytes;
 #[cfg(target_vendor = "teaclave")]
 use exception::ExceptionHandler;
+use http_body_util::{BodyExt, Empty};
 use lazy_static::lazy_static;
 use std::time::Duration;
 use tokio::runtime::{Builder, Runtime};
 use tokio::time::sleep;
 
+use hyper_rustls::HttpsConnector;
+use hyper_util::{
+    client::legacy::{connect::HttpConnector, Client},
+    rt::TokioExecutor,
+};
+use rustls::RootCertStore;
+use serde::Deserialize;
+
 lazy_static! {
     static ref RUNTIME: Runtime = Builder::new_multi_thread()
-        .worker_threads(4) // TCS = 4 * 2 (one for dns worker and one for decode) + 1 (reserved for initializer) = 7
+        .worker_threads(8) // TCS = 4 * 2 (one for dns worker and one for decode) + 1 (reserved for initializer) = 7
         .enable_all()
         .build()
         .unwrap();
@@ -53,34 +64,66 @@ pub unsafe extern "C" fn init_runtime() {
     });
 }
 
+#[derive(Debug, Deserialize, Clone)]
+pub struct TickerPrice {
+    pub amount: String,
+    pub base: String,
+    pub currency: String,
+}
+
+pub const COINBASE_TICKER_URL: &str = "https://api.coinbase.com/v2/prices/ETH-USD/spot";
+
+/// This version uses the `hyper_rustls` crate and the legacy `hyper` Client, which handles client management.
+/// I tested this only to investigate what aspect of the client might be incompatible with the enclave,
+/// but I wasn’t able to reproduce the error. I do NOT recommend using this approach, as I prefer the lower-level approach.
+pub async fn fetch_url() -> Result<()> {
+    println!("Entered fetch_url");
+    let root_store = RootCertStore {
+        roots: webpki_roots::TLS_SERVER_ROOTS.into(),
+    };
+
+    let config = rustls::ClientConfig::builder()
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+
+    let https = hyper_rustls::HttpsConnectorBuilder::new()
+        .with_tls_config(config)
+        .https_or_http()
+        .enable_http1()
+        .build();
+    // NOTE: This client is legacy, and I don't believe that we need it. See: https://hyper.rs/guides/1/client/connectors/
+    // I just wanted to check whether or not it worked for my own edification.
+    let client: Client<HttpsConnector<HttpConnector>, Empty<Bytes>> =
+        Client::builder(TokioExecutor::new()).build(https);
+
+    let url = COINBASE_TICKER_URL.parse::<hyper::Uri>().unwrap();
+
+    let res = client.get(url).await?;
+
+    println!("Response: {}", res.status());
+    println!("Headers: {:#?}\n", res.headers());
+
+    let bytes = res.into_body().collect().await?.to_bytes();
+    let body = String::from_utf8(bytes.to_vec()).context("Response body is not valid UTF-8")?;
+    // deserialize the response into a TickerPrice struct like this:
+    // {"data":{"amount":"96663.235","base":"BTC","currency":"USD"}}
+    let data: serde_json::Value =
+        serde_json::from_str(&body).context("Failed to parse JSON response")?;
+    let ticker_price: TickerPrice =
+        serde_json::from_str(&data["data"].to_string()).context("Failed to parse JSON response")?;
+    println!("ETH MARKETS: {:?}", ticker_price);
+    Ok(())
+}
+
 /// # Safety
 #[no_mangle]
 pub unsafe extern "C" fn spawn_http_request(count: u64) {
+    #[cfg(target_vendor = "teaclave")]
+    let _handle = ExceptionHandler::new().unwrap();
     let rt = &*RUNTIME.handle();
     let mut handles = Vec::new();
     for _ in 0..count {
-        let client = reqwest::Client::builder()
-            .tcp_keepalive(Duration::from_secs(1))
-            .build()
-            .unwrap();
-        let handle = rt.spawn(async move {
-            #[cfg(target_vendor = "teaclave")]
-            let _handle = ExceptionHandler::new().unwrap();
-            let res = {
-                // get some fun facts about cats
-                client
-                    .get("https://catfact.ninja/fact")
-                    .send()
-                    .await
-                    .unwrap()
-            };
-            println!("{:?}", res.text().await.unwrap());
-        });
+        let handle = rt.block_on(fetch_url());
         handles.push(handle);
     }
-    rt.block_on(async {
-        for handle in handles {
-            handle.await.unwrap();
-        }
-    });
 }
